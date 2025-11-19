1cb7103c40f7033ab04f765d4d0aa632bcfa98b7
RuyBarbosa22 <ruybarbao@gmail.com>
2025-11-19 09:37:16 -0300

    hotfix menebot blink

--- frontend/src/components/Menebot/Menebot.tsx
+++ frontend/src/components/Menebot/Menebot.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
-import menebotFrontImage from '/assets/images/menebotFront.png?url';
-import menebotBlinkImage from '/assets/images/menebotBlink.png?url';
-import wingLeftSvg from '/assets/icons/menebot-wing-left.svg?url';
-import wingRightSvg from '/assets/icons/menebot-wing-right.svg?url';
-import eyeSvg from '/assets/icons/menebot-eye.svg?url';
-import closedEyeSvg from '/assets/icons/menebot-closed-eye.svg?url';
+import menebotFrontImage from '/assets/images/menebotFront.png';
+import menebotBlinkImage from '/assets/images/menebotBlink.png';
+import wingLeftSvg from '/assets/icons/menebot-wing-left.svg';
+import wingRightSvg from '/assets/icons/menebot-wing-right.svg';
+import eyeSvg from '/assets/icons/menebot-eye.svg';
+import closedEyeSvg from '/assets/icons/menebot-closed-eye.svg';
 
 interface MenebotProps {
   className?: string;
